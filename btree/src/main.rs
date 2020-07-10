433cfd5ad2e7a2af449cc786843bcdd0fbd28045
ymt117 <yamamto117@gmail.com>
2020-07-10 13:07:03 +0900

    下記サイトを参考にしてノードをstruct型に変更 cloneメソッドとinsert関数を追加した
    https://qiita.com/quercus491/items/8cb62209617dcff2c9d0

--- btree/src/main.rs
+++ btree/src/main.rs
@@ -1,31 +1,51 @@
 #[derive(Debug)]
-enum BTreeNode {
-    Operator {
-        left_node: Box<Option<BTreeNode>>,
-        right_node: Box<Option<BTreeNode>>,
-    },
-    Number {
-        key: u32,
-        val: String,
-    },
+struct BTreeNode {
+    left_node: Box<Option<BTreeNode>>,
+    right_node: Box<Option<BTreeNode>>,
+    
+    key: u32,
+    val: String,
+}
+
+impl Clone for BTreeNode {
+    fn clone(&self) -> Self {
+        BTreeNode {
+            left_node: self.left_node.clone(),
+            right_node: self.right_node.clone(),
+
+            key: self.key,
+            val: self.val.clone(),
+        }
+    }
 }
 
 impl BTreeNode {
     fn new(key: u32, val: String) -> BTreeNode {
-        BTreeNode::Operator {
+        BTreeNode {
             left_node: Box::new(None),
             right_node: Box::new(None),
-        };
 
-        BTreeNode::Number {
             key: key,
             val: val,
         }
     }
 }
 
-fn insert(node: BTreeNode, key: u32, val: String) {
-    
+// ノードを追加する
+fn insert(node: Box<Option<BTreeNode>>, key: u32, val: String) -> Box<Option<BTreeNode>> {
+    return match *node {
+        None => Box::new(Some(BTreeNode::new(key, val))),
+        Some(v) => {
+            let mut target_node = v.clone();
+            if key < target_node.key {
+                target_node.left_node = insert(target_node.left_node, key, val);
+            }
+            else {
+                target_node.right_node = insert(target_node.right_node, key, val);
+            }
+            Box::new(Some(target_node))
+        }
+    };
 }
 
 fn find(key: u32) -> String {
@@ -33,7 +53,13 @@ fn find(key: u32) -> String {
 }
 
 fn main() {
-    
+    let tree = BTreeNode::new(1234, "aaa".to_string());
+    println!("{:#?}", tree);
+}
+
+#[test]
+fn test_insert() {
+
 }
 
 #[test]
